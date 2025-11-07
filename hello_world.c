297905718b353a6c0dd7eb99de25709473def7ac
Vishwa S <vishwa15.11.07@gmail.com>
2025-11-07 18:10:46 +0530

    Add Week 1 program: hello_world.c
    Uploaded my first two C programs as part of Week 1 — learning syntax, input/output.

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+// Program: A simple program to print "Hello, World!" to the console.
+//Author: Vishwa S
+//Date: 2024-06-15
+
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
